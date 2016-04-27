46ed52c905a9e9ed1a2cf453b6558f02acbabedc
Philip <Philip@134.108.242.117>
2016-04-27 16:12:35 +0200

    verwirrendes zeug beim commiten

--- src/database/DataBaseConnector.java
+++ src/database/DataBaseConnector.java
@@ -1,34 +1,38 @@
 package database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 import exceptions.DataBasePathNotFoundException;
 
-public class DataBaseConnector 
+public class DataBaseConnector
 {
-	public String path = "";//"C:/Users/Philip/workspace/test/LabTallySystem.sqlite";
+	public String path = "";
 
 	public DataBaseConnector(String inputpath)
 	{
 		path = inputpath;
 	}
-	
+
 	public Connection ConnectToDataBase() throws DataBasePathNotFoundException
-	{		
+	{
 		Connection connection = null;
-		try{	
-			if(path != ""){
+		try
+		{
+			if (path != "")
+			{
 				Class.forName("org.sqlite.JDBC");
-			    connection = DriverManager.getConnection("jdbc:sqlite:"+path);		
-			    return connection;
-			}
-			else throw new DataBasePathNotFoundException();
-		}
-		catch(ClassNotFoundException c){
+				connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+				return connection;
+			} else
+				throw new DataBasePathNotFoundException();
+		} catch (ClassNotFoundException c)
+		{
 			System.out.println(c.getMessage());
 			throw new DataBasePathNotFoundException();
-		}
-		catch(SQLException s){
+		} catch (SQLException s)
+		{
 			System.out.println(s.getMessage());
 			throw new DataBasePathNotFoundException();
 		}

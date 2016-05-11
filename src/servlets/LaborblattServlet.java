323c8873bb56aeb01c56abb5a138629ebb4b4868
User <C.Gschrey@gmx.net>
2016-05-11 15:59:39 +0200

    Neuer Stil

--- src/servlets/LaborblattServlet.java
+++ src/servlets/LaborblattServlet.java
@@ -19,26 +19,16 @@ import logic.LaborblattLogic;
 import objects.Aufgabe;
 import objects.LaborblattView;
 
-/**
- * Servlet implementation class LaborblattServlet
- */
 @WebServlet("/LaborblattServlet")
 public class LaborblattServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public LaborblattServlet()
 	{
 		super();
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		response.setContentType("text/html");
@@ -138,10 +128,6 @@ public class LaborblattServlet extends HttpServlet
 		out.close();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		// TODO Auto-generated method stub

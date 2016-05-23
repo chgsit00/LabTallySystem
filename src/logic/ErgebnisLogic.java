d21b1c732c179b51b6b6e3d5bb2e12d6c54edd0f
User <C.Gschrey@gmx.net>
2016-05-23 14:00:36 +0200

    File-Upload funktioniert jetzt

--- src/logic/ErgebnisLogic.java
+++ src/logic/ErgebnisLogic.java
@@ -1,5 +1,9 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,6 +19,51 @@ import objects.Rechner;
 
 public class ErgebnisLogic
 {
+	public List<String> HandleErgebnis(String teamNr, String aufgabeNr, String rechnerNr, InputStream input)
+			throws DataBasePathNotFoundException, NoAccessToDataBaseException
+	{
+		List<String> message = new ArrayList<String>();
+		RechnerManagement rechnerManagement = new RechnerManagement();
+		Rechner rechner = rechnerManagement.GetRechner_by_RechnerNr(rechnerNr);
+		if (rechner.RechnerNr == null)
+		{
+			message.add("Rechner with RechnerNr " + rechnerNr + " does not exist. Please check your Login");
+		} else
+		{
+			LoesungManagement loesungManagement = new LoesungManagement();
+			Loesung loesung = loesungManagement.GetLoesung_by_RechnerNr_and_AufgabeNr(aufgabeNr, rechnerNr);
+			if (loesung.AufgabeNr == null)
+			{
+				message.add("Loesung with RechnerNr " + rechnerNr + " and AufgabeNr " + aufgabeNr + " does not exist");
+			} else
+			{
+				String eingabe = GetFileContent(input);
+				boolean bestanden = Compare_Ergebnis_to_Loesung(loesung.Loesung, eingabe);
+				ErgebnisManagement ergebnisManagement = new ErgebnisManagement();
+				Ergebnis ergebnis = ergebnisManagement.GetErgebnis_by_AufgabeNr_and_TeamNr(aufgabeNr, teamNr);
+				if (ergebnis.AufgabeNr == null || ergebnis.TeamNr == null)
+				{
+					message.add(ergebnisManagement.SaveErgebnis(teamNr, aufgabeNr, rechnerNr, eingabe, bestanden,
+							(new Date()).toString()));
+				} else
+				{
+					if (ergebnis.Bestanden == false)
+					{
+						ergebnisManagement.DeleteErgebnis(teamNr, aufgabeNr);
+						message.add(ergebnisManagement.SaveErgebnis(teamNr, aufgabeNr, rechnerNr, eingabe, bestanden,
+								(new Date()).toString()));
+					} else
+					{
+						bestanden = true;
+						message.add("You already passed that Labtask - There's no Need to change the result");
+					}
+				}
+				message.add("Passed = " + bestanden);
+			}
+		}
+		return message;
+	}
+
 	public List<String> HandleErgebnis(String teamNr, String aufgabeNr, String rechnerNr, String eingabe)
 			throws DataBasePathNotFoundException, NoAccessToDataBaseException
 	{
@@ -66,5 +115,37 @@ public class ErgebnisLogic
 			return true;
 		} else
 			return false;
+	}
+
+	private String GetFileContent(InputStream file)
+	{
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+		StringBuilder out = new StringBuilder();
+		String line;
+		try
+		{
+			while ((line = reader.readLine()) != null)
+			{
+				out.append(line);
+			}
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				reader.close();
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println(out.toString()); // Prints the string content read
+											// from input stream
+		return out.toString();
 	}
 }
--- src/servlets/LaborblattServlet.java
+++ src/servlets/LaborblattServlet.java
@@ -1,16 +1,24 @@
 package servlets;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 import exceptions.DataBasePathNotFoundException;
 import exceptions.NoAccessToDataBaseException;
@@ -20,13 +28,21 @@ import objects.Aufgabe;
 import objects.LaborblattView;
 
 @WebServlet("/LaborblattServlet")
+@MultipartConfig
 public class LaborblattServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+	private static final String UPLOAD_DIRECTORY = "upload";
+	private static final int THRESHOLD_SIZE = 1024 * 1024 * 3; // 3MB
+	private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
+	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; // 50MB
+
+	private List<String> UsedUploadFields;
 
 	public LaborblattServlet()
 	{
 		super();
+		UsedUploadFields = new ArrayList<String>();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -43,7 +59,6 @@ public class LaborblattServlet extends HttpServlet
 			if (rechnerNr == null || teamNr == null)
 			{
 				out.print("<p>You must be signed in to see this page</p>");
-				out.println("<p><a href='/LabTallySystem/'>zur&uuml;ck</a></p>");
 			} else
 			{
 				LaborblattLogic laborblattlogic = new LaborblattLogic();
@@ -82,32 +97,40 @@ public class LaborblattServlet extends HttpServlet
 
 						out.println("<div id=\"Blatt" + iterator + "\" class = \"Laborblatt Blatt" + iterator + "\">");
 						out.println("<h3> Laborblatt: " + iterator + "</h3>");
+						out.println(
+								"<form action=\"LaborblattServlet\" method=\"post\" enctype=\"multipart/form-data\">");//
 						for (Aufgabe aufgabe : laborblatt.Aufgaben)
 						{
 							out.println("<div id=\"Aufgabe" + aufgabe.AufgabeNr + "\" class = \"Aufgabe\">");
 							out.println("<h4> Aufgabe " + aufgabe.AufgabeNr + "</h4>");
 							out.println("<p>" + aufgabe.AufgabeText + "</p>");
-							out.println("<form action=\"LaborblattServlet\" method=\"post\">");
-							out.println(
-									"Your Answer: <input type=\"text\" name=\"eingabe" + aufgabe.AufgabeNr + "\"><br>");
-							out.println("<input type=\"submit\" value=\"Check Answer\">");
-							out.println("</form>");
-							String eingabe = request.getParameter("eingabe" + aufgabe.AufgabeNr);
-							List<String> messages = ergebnislogic.HandleErgebnis(teamNr, aufgabe.AufgabeNr, rechnerNr,
-									eingabe);
-							out.println("<div class = \"Message\">");
-							for (String message : messages)
+							if (aufgabe.EingabeArt.equals("ManualInput"))
 							{
-								out.println("<p>" + message + "</p>");
+								out.println("Your Answer: <input type=\"text\" name=\"eingabe" + aufgabe.AufgabeNr
+										+ "\"><br>");
+								String eingabe = request.getParameter("eingabe" + aufgabe.AufgabeNr);
+								List<String> messages = ergebnislogic.HandleErgebnis(teamNr, aufgabe.AufgabeNr,
+										rechnerNr, eingabe);
+								out.println("<div class = \"Message\">");
+								for (String message : messages)
+								{
+									out.println("<p>" + message + "</p>");
+								}
+								out.println("</div>");
+							} else if (aufgabe.EingabeArt.equals("Upload"))
+							{
+								out.println("Select file to upload: <input type=\"file\" name=\"" + aufgabe.AufgabeNr
+										+ "\" />");
+								UsedUploadFields.add(aufgabe.AufgabeNr);
 							}
 							out.println("</div>");
-							out.println("</div>");
 						}
+						out.println("<input type=\"submit\" value=\"Check Answers\">");
+						out.println("</form>"); //
 						out.println("</div>");
 						iterator++;
 					}
 					out.println("</body>");
-					out.println("<a href='/LabTallySystem/'>zur&uuml;ck</a>");
 					out.println("</html>");
 				} catch (DataBasePathNotFoundException e)
 				{
@@ -123,14 +146,100 @@ public class LaborblattServlet extends HttpServlet
 		} else
 		{
 			out.print("You must be signed in to see this page");
-			out.println("<a href='/LabTallySystem/login.html'>zur&uuml;ck</a>");
 		}
 		out.close();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		if (session != null)
+		{
+			String rechnerNr = (String) session.getAttribute("rechnerNr");
+			String teamNr = (String) session.getAttribute("teamNr");
+
+			// checks if the request actually contains upload file
+			if (!ServletFileUpload.isMultipartContent(request))
+			{
+				PrintWriter writer = response.getWriter();
+				writer.println("Request does not contain upload data");
+				writer.flush();
+				return;
+			}
+
+			ErgebnisLogic ergebnisLogic = new ErgebnisLogic();
+			// configures upload settings
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			factory.setSizeThreshold(THRESHOLD_SIZE);
+			factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			upload.setFileSizeMax(MAX_FILE_SIZE);
+			upload.setSizeMax(MAX_REQUEST_SIZE);
+
+			// constructs the directory path to store upload file
+			String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+			// creates the directory if it does not exist
+			File uploadDir = new File(uploadPath);
+			if (!uploadDir.exists())
+			{
+				uploadDir.mkdir();
+			}
+
+			for (String uploadField : UsedUploadFields)
+			{
+				try
+				{
+					Part filePart = request.getPart(uploadField); // Retrieves
+																	// <input
+					// type="file"
+					// name="file">
+					InputStream filecontent = filePart.getInputStream();
+					System.out.println(uploadField);
+					List<String> messages = ergebnisLogic.HandleErgebnis(teamNr, uploadField, rechnerNr, filecontent);
+					for (String message : messages)
+					{
+						System.out.println(message);
+					}
+					// parses the request's content to extract file data
+					// List<FileItem> formItems =
+					// upload.parseRequest((RequestContext)
+					// request);
+					// Iterator<FileItem> iter = formItems.iterator();
+					// ErgebnisLogic ergebnisLogic = new ErgebnisLogic();
+					// // iterates over form's fields
+					// while (iter.hasNext())
+					// {
+					// System.out.println("Hier3"); //
+					// FileItem item = (FileItem) iter.next();
+					// // processes only fields that are not form fields
+					// if (!item.isFormField())
+					// {
+					// System.out.println("Hier4"); //
+					// String fileName = new File(item.getName()).getName();
+					// String filePath = uploadPath + File.separator + fileName;
+					// File storeFile = new File(filePath);
+					// String fieldName = item.getName();
+					// System.out.println(fieldName);
+					// // List<String> messages =
+					// // ergebnisLogic.HandleErgebnis(teamNr, aufgabeNr,
+					// // rechnerNr, input)
+					//
+					// // saves the file on disk
+					// item.write(storeFile);
+					//
+					// }
+					// }
+					request.setAttribute("message", "Upload has been done successfully!");
+				} catch (Exception ex)
+				{
+					System.out.println("There was an error: " + ex.getMessage());
+					request.setAttribute("message", "There was an error: " + ex.getMessage());
+				}
+			}
+			// getServletContext().getRequestDispatcher("/message.jsp").forward(request,
+			// response);
+		}
 		doGet(request, response);
 	}
 

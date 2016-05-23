13f935c6a53d31ab7765416f2542ca646dc2e2e4
Philip <dario-f@web.de>
2016-05-23 14:46:00 +0200

    Ranking wird jetzt auch nach Zeitstempel sortiert

--- src/logic/TeamOverviewLogic.java
+++ src/logic/TeamOverviewLogic.java
@@ -1,10 +1,13 @@
 package logic;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 import exceptions.DataBasePathNotFoundException;
 import exceptions.NoAccessToDataBaseException;
@@ -32,6 +35,25 @@ public class TeamOverviewLogic
 		return iterator;
 	}
 
+	private LocalDateTime GetNewestTimestamp_for_Team(List<Ergebnis> ergebnisList)
+	{
+		LocalDateTime timestamp = LocalDateTime.MIN;
+		for (Ergebnis ergebnis : ergebnisList)
+		{
+			if (ergebnis.Bestanden == true)
+			{
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+				LocalDateTime date = LocalDateTime.parse(ergebnis.Zeitstempel, formatter);
+
+				if (date.isAfter(timestamp))
+				{
+					timestamp = date;
+				}
+			}
+		}
+		return timestamp;
+	}
+
 	public List<TeamOverview> GetTeamOverview()
 			throws DataBasePathNotFoundException, NoAccessToDataBaseException, SQLException
 	{
@@ -46,6 +68,7 @@ public class TeamOverviewLogic
 		{
 			List<Ergebnis> teamErgebnisse = GetErgebnisse_forTeamNr(team.TeamNr, ergebnisse);
 			int bestandeneAufgaben = GetSolvedTasks_for_Team(teamErgebnisse);
+			LocalDateTime localDateTime = GetNewestTimestamp_for_Team(teamErgebnisse);
 			List<AufgabenDisplay> aufgabentexte = new ArrayList<AufgabenDisplay>();
 			for (Aufgabe aufgabe : aufgaben)
 			{
@@ -54,15 +77,27 @@ public class TeamOverviewLogic
 						bestanden);
 				aufgabentexte.add(aufgabenDisplay);
 			}
-			teamOverviewList.add(new TeamOverview(team.TeamNr, "" + bestandeneAufgaben, aufgabentexte));
+			teamOverviewList.add(new TeamOverview(team.TeamNr, "" + bestandeneAufgaben, aufgabentexte, localDateTime));
 		}
 		Collections.sort(teamOverviewList, new Comparator<TeamOverview>()
 		{
 			@Override
 			public int compare(TeamOverview overView2, TeamOverview overView1)
 			{
-
-				return overView1.BestandeneAufgabeCount.compareTo(overView2.BestandeneAufgabeCount);
+				int c;
+				c = overView1.BestandeneAufgabeCount.compareTo(overView2.BestandeneAufgabeCount);
+				if (c == 0)
+					if (overView1.localDateTime.isAfter(overView2.localDateTime))
+					{
+						c = -1;
+					} else if (overView1.localDateTime.isEqual(overView2.localDateTime))
+					{
+						c = 0;
+					} else if (overView1.localDateTime.isBefore(overView2.localDateTime))
+					{
+						c = 1;
+					}
+				return c;
 			}
 		});
 		return teamOverviewList;

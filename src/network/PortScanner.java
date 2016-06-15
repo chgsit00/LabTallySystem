1509e01e22587a19a4640bf24901207ef8b23ea6
Philip <dario-f@web.de>
2016-06-15 11:47:50 +0200

    unnötiger commit

--- src/network/PortScanner.java
+++ src/network/PortScanner.java
@@ -81,7 +81,7 @@ public class PortScanner
 	{
 		// List<Future<Port>> ports = ScanPorts("192.168.0.1");
 		// PrintPorts(ports);
-		SimplePortScan("localhost");
+		SimplePortScan("10.0.2.2");
 		// NMapScan();
 	}
 
